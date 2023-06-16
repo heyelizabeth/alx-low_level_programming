61dba8942e289994d57b47843c4d423dc257b862
heyElizabeth <helloaleeyah@gmail.com>
2023-06-16 05:01:10 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,29 @@
-#include <stdio.h>>
-/**
- * main - A program that prints the size of various computer types
- * Return 0 (Success)
- */
-int main(void)
-{
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+#include<stdio.h>
 
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf
+/**
+
+* main - Entry point
+
+*
+
+* Return: Always 0 (Success)
+
+*/
+
+int main(void)
+
+{
+
+        printf("Size of a char: %d byte(s)\n", sizeof(char));
+
+        printf("Size of an int: %d byte(s)\n", sizeof(int));
+
+        printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+
+        printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+
+        printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+        return (0);
+
+}
