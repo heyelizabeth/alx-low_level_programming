ea2975787fd1fe9ae92cf204bbe0d8fd3b3634f5
heyElizabeth <helloaleeyah@gmail.com>
2023-06-23 12:52:21 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
- */
+* print_line - draws a straight line in the terminal
+* @n: number of times the character _ should be printed
+*/
 void print_line(int n)
 {
 if (n <= 0)
 {
 _putchar('\n');
-} else
+} 
+else
 {
 int i;
 for (i = 1; i <= n; i++)
