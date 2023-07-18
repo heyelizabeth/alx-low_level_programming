ece3cb39201e08af0d421e314ee66f636ca06721
heyElizabeth <helloaleeyah@gmail.com>
2023-07-18 15:51:29 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,22 @@
+#ifndef _0_OBJECT_LIKE_MACRO_H_
+#define _0_OBJECT_LIKE_MACRO_H_
+
+
+#define SIZE 1024
+
+
+#endif
+
+
+=== 1 - pi.h===
+
+
+#ifndef _PI_H_
+#define _PI_H_
+
+
+#define PI 3.14159265359
+
+
+#endif
+
